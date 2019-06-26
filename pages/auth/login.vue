7099d662469ca1aaa65933295fc7783180dc65c1
Bob Weichler <bobmasaki@hotmail.com>
2019-06-26 09:43:27 +0200

    initial commit

---
+++ pages/auth/login.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="auth-container">
+      Login Page
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'Login'
+   }
+</script>
---
+++ pages/auth/register.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="auth-container">
+      Register Component
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'Register'
+   }
+</script>
